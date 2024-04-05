584ecb7c6943926761e33e35d37682c77a1fd00a
Shivam Panchal <shivampanchal@Shivams-MacBook-Air.local>
2024-04-05 00:49:02 -0700

    Almost Done

---
+++ src/utils/images.js
@@ -0,0 +1,5 @@
+import shopping_cart from "../assets/images/shopping_cart.png";
+import correct from "../assets/images/correct.png";
+import loader from "../assets/images/loader.svg";
+ 
+export {shopping_cart, correct, loader};
